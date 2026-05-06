9d50599b5a75aa8274318c144874bfda43e8e8fa
your name <kossi.sedoufia@student.swic.edu>
2026-05-05 23:53:14 -0500

    Try2: Add resp.ok check for error handling

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 console.log("Hello world!");
+
 async function getPlanet(id) {
   const resp = await fetch(`https://swapi.info/api/planets/${id}`);
+
+  if (!resp.ok) {
+    console.error(`Request failed: ${resp.status}`);
+    return;
+  }
+
   const data = await resp.json();
   console.info(data);
 }
 
-getPlanet(1);
+getPlanet(999);
