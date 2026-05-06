1365bb66c7352aaf85c53a6f7c91cc1c295e57de
your name <kossi.sedoufia@student.swic.edu>
2026-05-05 23:33:43 -0500

    Try1: Fetch a planet with await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+async function getPlanet(id) {
+  const resp = await fetch(`https://swapi.info/api/planets/${id}`);
+  const data = await resp.json();
+  console.info(data);
+}
+
+getPlanet(1);
